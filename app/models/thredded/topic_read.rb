e56902c0fca441506044cecfb7bee65205f21c18
Joel Oliveira <joel.oliveira@gmail.com>
2016-05-22 14:58:24 -0400

    [JRO] Update TopicRead model
    Add a `where` class method that will find topics and return their read
    and unread state

--- app/models/thredded/topic_read.rb
+++ app/models/thredded/topic_read.rb
@@ -8,6 +8,7 @@ module Thredded
 
       $redis.hmset(
         "thredded:reads:#{topic_id}",
+        'topic_id', topic_id,
         'furthest_post', furthest_post,
         'furthest_page', furthest_page,
         'done_reading', done_reading
@@ -18,6 +19,33 @@ module Thredded
       $redis
         .hgetall("thredded:reads:#{topic_id}")
         .with_indifferent_access
+    end
+
+    def self.where(topic_ids:)
+      found = $redis.pipelined do
+        topic_ids.each do |topic_id|
+          $redis.hgetall "thredded:reads:#{topic_id}"
+        end
+      end
+
+      read   = map_reduce_to_hash(found.select { |data| data['done_reading'] == 'true' })
+      unread = map_reduce_to_hash(found.select { |data| data['done_reading'] == 'false' })
+
+      {
+        read: read,
+        unread: unread
+      }
+    end
+
+    def self.map_reduce_to_hash(topic_data)
+      topic_data.map! do |data|
+        topic_state = Hash.new
+        topic_state[data['topic_id']] = {
+          furthest_post: data['furthest_post'],
+          furthest_page: data['furthest_page']
+        }
+        topic_state
+      end.reduce({}, :merge)
     end
   end
 end
--- spec/models/thredded/topic_read_spec.rb
+++ spec/models/thredded/topic_read_spec.rb
@@ -28,9 +28,10 @@ RSpec.describe Thredded::TopicRead, redis: true do
         done_reading: 'false'
       }
       expected_results = HashWithIndifferentAccess.new(
-        furthest_post: '10',
+        done_reading: 'false',
         furthest_page: '5',
-        done_reading: 'false'
+        furthest_post: '10',
+        topic_id: '1',
       )
 
       Thredded::TopicRead.upsert(params)
@@ -52,13 +53,61 @@ RSpec.describe Thredded::TopicRead, redis: true do
         done_reading: 'true'
       }
       expected_results = HashWithIndifferentAccess.new(
-        furthest_post: '101',
+        done_reading: 'true',
         furthest_page: '3',
-        done_reading: 'true'
+        furthest_post: '101',
+        topic_id: '2',
       )
       Thredded::TopicRead.upsert(params)
 
       expect(Thredded::TopicRead.find(2)).to eq(expected_results)
+    end
+  end
+
+  describe '.where(topic_ids:)' do
+    it 'returns the correct json for the data' do
+      $redis.pipelined do
+        $redis.hmset('thredded:reads:2',
+                     'topic_id', '2',
+                     'furthest_post', '10',
+                     'furthest_page', '3',
+                     'done_reading', 'true')
+        $redis.hmset('thredded:reads:3',
+                     'topic_id', '3',
+                     'furthest_post', '20',
+                     'furthest_page', '3',
+                     'done_reading', 'false')
+        $redis.hmset('thredded:reads:4',
+                     'topic_id', '4',
+                     'furthest_post', '30',
+                     'furthest_page', '3',
+                     'done_reading', 'true')
+      end
+
+      expected_results = HashWithIndifferentAccess.new(
+        read: {
+          '2': {
+            furthest_post: '10',
+            furthest_page: '3'
+          },
+          '4': {
+            furthest_post: '30',
+            furthest_page: '3'
+          }
+        },
+        unread: {
+          '3': {
+            furthest_post: '20',
+            furthest_page: '3'
+          }
+        }
+      )
+
+      read_and_unread = HashWithIndifferentAccess.new(
+        Thredded::TopicRead.where(topic_ids: %w(2 3 4))
+      )
+
+      expect(read_and_unread).to eq(expected_results)
     end
   end
 end
