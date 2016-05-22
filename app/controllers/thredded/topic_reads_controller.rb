c314ef01235ede33db696c181f657ab7aa35d40e
Joel Oliveira <joel.oliveira@gmail.com>
2016-05-22 14:57:52 -0400

    [JRO] List read and unread topics via json

--- app/controllers/thredded/topic_reads_controller.rb
+++ app/controllers/thredded/topic_reads_controller.rb
@@ -1,5 +1,5 @@
 module Thredded
-  class TopicReadsController < ::ApplicationController
+  class TopicReadsController < Thredded::ApplicationController
     def update
       Thredded::TopicRead.upsert(read_params)
 
@@ -7,9 +7,16 @@ module Thredded
     end
 
     def index
+      read_and_unread = Thredded::TopicRead.where(topic_ids: topic_ids)
+
+      render json: read_and_unread
     end
 
     private
+
+    def topic_ids
+      params[:topic_ids].split('/')
+    end
 
     def read_params
       require(:topic_read)
